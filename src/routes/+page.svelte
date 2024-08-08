411796df1848f9c6ba08816d57fb238fa989f783
Maaz <dev.maaztariq@gmail.com>
2024-08-08 15:04:15 +0500

    _2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,7 +9,7 @@ As a dedicated indie game studio, we take pride in our relentless pursuit of cre
 
 <p>
 
-Our team,
+Our team ,
 comprised of talented individuals driven by a shared love for gaming, strives to craft immersive worlds and captivating narratives that resonate with players worldwide. 
 At Torsion Interactive, we believe in the power of storytelling and the transformative potential of games.
 Join us on this exciting journey as we redefine the boundaries of indie gaming and create unforgettable, groundbreaking adventures for a diverse and global audience.  
